e5169e5f2253c70df5179448ca1a2735839e48c4
qazxsw99 <qazxsw99@outlook.com>
2014-02-14 11:07:30 +0800

    * Commit Test

--- WriteFileDemo/Program.cs
+++ WriteFileDemo/Program.cs
@@ -9,6 +9,7 @@ namespace WriteFileDemo {
     class Program {
         static void Main(string[] args) {
             // test 123
+            // 456
             var log = args.Length > 0 ? string.Join(", ", args) : "Empty";
             File.WriteAllText(@"D:/_writeFileDemo.txt", string.Format("{0}, {1}", DateTime.Now, log));
         }
