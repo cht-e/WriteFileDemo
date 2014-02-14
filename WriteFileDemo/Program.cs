e96ac0f1851ad5fe992b08173acbded7869f927f
qazxsw99 <qazxsw99@outlook.com>
2014-02-14 11:02:02 +0800

    * Init

---
+++ WriteFileDemo/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WriteFileDemo {
+    class Program {
+        static void Main(string[] args) {
+            // test 123
+            var log = args.Length > 0 ? string.Join(", ", args) : "Empty";
+            File.WriteAllText(@"D:/_writeFileDemo.txt", string.Format("{0}, {1}", DateTime.Now, log));
+        }
+    }
+}
