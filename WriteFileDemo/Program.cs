04f1e86cf277b3902da314492ccd3cf72e0f3bd3
qazxsw99 <qazxsw99@gmail.com>
2014-02-14 11:26:14 +0800

    Delete useless comments

--- WriteFileDemo/Program.cs
+++ WriteFileDemo/Program.cs
@@ -7,12 +7,10 @@ using System.Threading.Tasks;
 
 namespace WriteFileDemo {
     class Program {
-        static void Main(string[] args) {
-            // test 123
-            // 456
-            // beta 1
+        static void Main(string[] args) {            
+            // v1.0
             var log = args.Length > 0 ? string.Join(", ", args) : "Empty";
-            File.WriteAllText(@"D:/_writeFileDemo.txt", string.Format("{0}, {1}", DateTime.Now.ToUniversalTime(), log));
+            File.WriteAllText(@"D:/_writeFileDemo.txt", string.Format("{0}, {1}", DateTime.Now, log));
         }
     }
 }
